afe1b023f03f633af2bb9ed130e9b17c7ced4780
Melvin Whitaker <awsome7mailman@gmail.com>
2024-03-04 14:52:05 -0700

    Created Password Hashing!

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -3,6 +3,7 @@ package service;
 import Model.Auth;
 import Model.User;
 import dataAccess.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Objects;
 import java.util.UUID;
@@ -23,7 +24,10 @@ public class UserService {
         }
         String username = user.getUsername();
         if(userDAO.getUser(username)==null){
-            userDAO.createUser(user);
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            String hashedPassword = encoder.encode(user.getPassword());
+            User hashedUser = new User(user.getUsername(), hashedPassword, user.getEmail());
+            userDAO.createUser(hashedUser);
             return createNewAuth(username);
         }else {
             throw new DataAccessException("Already Taken!");
@@ -33,11 +37,14 @@ public class UserService {
     public Auth login(User user) throws DataAccessException {
         String username = user.getUsername();
         User getUser = userDAO.getUser(username);
-        if((getUser!=null)&&(Objects.equals(user.getPassword(), getUser.getPassword()))){
-            return createNewAuth(username);
-        }else {
-            throw new DataAccessException("Unauthorized!");
+        if(getUser!=null) {
+            var hashedPassword = getUser.getPassword();
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            if(encoder.matches(user.getPassword(), hashedPassword)){
+                return createNewAuth(username);
+            }
         }
+        throw new DataAccessException("Unauthorized!");
     }
 
     private Auth createNewAuth(String username) {
