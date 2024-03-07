1f1956f92659488c259386b55f9d5bbd4f41bdf3
Melvin Whitaker <awsome7mailman@gmail.com>
2024-03-07 16:37:56 -0700

    Refactored Database Manager.

--- server/src/main/java/dataAccess/DatabaseManager.java
+++ server/src/main/java/dataAccess/DatabaseManager.java
@@ -67,7 +67,6 @@ public class DatabaseManager {
             throw new DataAccessException(e.getMessage());
         }
     }
-
     public void initializeDatabase() throws DataAccessException {
         configureDatabase();
     }
