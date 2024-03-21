80acc39da4253b3c008d46fcc2ab8c332365f279
Melvin Whitaker <awsome7mailman@gmail.com>
2024-03-21 12:25:20 -0600

    Minor Visual Change When Drawing Board.

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -198,15 +198,20 @@ public class Client {
 
     private void clientDrawChessBoard(Game game, Join join) {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-        ChessBoardUI.Perspective persp;
+        ChessBoardUI.Perspective persp, otherpersp;
+
         if(Objects.equals(join.getPlayerColor(), "WHITE")){
             persp = WHITE;
+            otherpersp = BLACK;
         }else if(Objects.equals(join.getPlayerColor(), "BLACK")){
             persp = BLACK;
+            otherpersp = WHITE;
         }else{
             persp = WATCH;
+            otherpersp = BLACK;
         }
         ChessBoardUI.drawChessBoard(out,game.getGame(),persp);
+        ChessBoardUI.drawChessBoard(out,game.getGame(),otherpersp);
     }
 
     private void postLoginUI() {
