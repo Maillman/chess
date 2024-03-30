327bd529bc0091a562351da6ea0d4ca80685910d
Melvin Whitaker <awsome7mailman@gmail.com>
2024-03-30 16:03:25 -0600

    Added WebSocketHandler and Connections...

---
+++ client/src/main/java/client2server/WebSocket/Connection.java
@@ -0,0 +1,19 @@
+package client2server.WebSocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.io.IOException;
+
+public class Connection {
+    public String userName;
+    public Session session;
+
+    public Connection(String userName, Session session) {
+        this.userName = userName;
+        this.session = session;
+    }
+
+    public void send(String msg) throws IOException {
+        session.getRemote().sendString(msg);
+    }
+}
---
+++ client/src/main/java/client2server/WebSocket/WebSocketHandler.java
@@ -0,0 +1,30 @@
+package client2server.WebSocket;
+
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.*;
+import org.eclipse.jetty.websocket.api.annotations.*;
+import webSocketMessages.serverMessages.ServerMessage;
+import webSocketMessages.userCommands.UserGameCommand;
+
+import java.io.IOException;
+import java.util.Timer;
+
+@WebSocket
+public class WebSocketHandler {
+    private final ConnectionManager connections = new ConnectionManager();
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+        UserGameCommand userGameCommand = new Gson().fromJson(message, UserGameCommand.class);
+        switch (userGameCommand.getCommandType()){
+            case JOIN_PLAYER -> join(userGameCommand.getAuthString(), session);
+        }
+    }
+
+    private void join(String authToken, Session session) throws IOException {
+        connections.add(authToken,session);
+        var message = String.format("%s has joined the game.",authToken);
+        var serverMessage = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
+        connections.broadcast(authToken, serverMessage);
+    }
+}
